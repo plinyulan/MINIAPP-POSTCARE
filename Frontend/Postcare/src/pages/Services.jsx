2feff588cd07813f5f0d8ede6dbffd3871a773a2
plinyulan <patty.linyulan@gmail.com>
2026-03-22 17:18:24 +0700

    test services page

--- Frontend/Postcare/src/pages/Services.jsx
+++ Frontend/Postcare/src/pages/Services.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Service.css";
+import "./Services.css";
 import { useNavigate } from "react-router-dom";
 import profileImg from "../img/profile.jpg";
 import homeIcon from "../img/home.png";
