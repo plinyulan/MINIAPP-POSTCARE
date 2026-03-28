863d15d5189a8668c34188b78b2f5a001251b813
plinyulan <patty.linyulan@gmail.com>
2026-03-28 16:29:19 +0700

    home page

--- Frontend/Postcare/src/pages/Home.jsx
+++ Frontend/Postcare/src/pages/Home.jsx
@@ -177,7 +177,7 @@ export default function Home() {
               <div
                 key={day.iso}
                 className={`calendar-item ${isLast ? "clickable" : "disabled"}`}
-                onClick={isLast ? () => navigate("/calendar") : undefined}
+                onClick={isLast ? () => navigate("/service") : undefined}
               >
                 <div className={`calendar-circle ${isLast ? "today" : ""}`}>
                   {day.date}
